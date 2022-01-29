ff3dbb2a0509df536eb88e62d389186559c31fdd
wellbrez <bld-wellington@hotmail.com>
2022-01-28 22:10:56 -0300

    update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Ensaios from "./Components/Ensaios";
 import Navbar from "./Components/Navbar";
 import ElementoDiferencial from "./Components/ElementoDiferencial";
 import CanvasEsquerdo from "./Components/CanvasEsquerdo";
+import DiagramaMohrCoulomb from "./Components/DiagramaMohrCoulomb";
 
-let propriedades = { sigmax: 50, sigmay: -50, tauxy: 50 };
+let propriedades = { sigmax: 500, sigmay: -50, tauxy: 500 };
+let ensaios = { compr: 100, trac: 100 };
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" on>
       <Navbar />
       <Ensaios />
       <CanvasEsquerdo>
         <ElementoDiferencial propriedades={propriedades} />
       </CanvasEsquerdo>
+      <DiagramaMohrCoulomb ensaios={ensaios} />
     </div>
   );
 }
